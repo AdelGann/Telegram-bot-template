4aeaa46005334fc674ab56c9ecccdc620b225d1d
Adel Chafic Gannem <121465465+AdelGann@users.noreply.github.com>
2024-05-05 14:36:59 -0400

    Update variables.js

--- configs/variables.js
+++ configs/variables.js
@@ -1,8 +1,5 @@
-let startMsg = [ // edit this message, this will be randomized
-    "En que puedo ayudarte hoy?\nPor favor escribe /help para ver mis demas funciones",
-    "Que tal tu dia, En que puedo servirte?\nor favor escribe /help para ver mis demas funciones",
-    "En que puedo ayudarte hoy?\nPor favor escribe /help para ver mis demas funciones"
-] 
+let startMsg = "En que puedo ayudarte hoy?\nPor favor escribe /help para ver mis demas funciones",
+
 let listOfCommands = [
     "",
     "/help - Lista de comandos\n",
